5227213bda8461eb55a559912e84e19d7ba15e1f
Danilo Silva Rodrigues <danilo.roddrigues@gmail.com>
2023-10-03 18:46:14 -0300

    handlerSubmit in TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSumit = (e) => {
+    e.preventDefault();
+    console.log("Value: ", value, "Categry: ", category);
+  };
+
   return (
     <div className="todo-form">
       <h2>Criar Tarefa:</h2>
-      <form>
-        <input type="text" placeholder="Digite o título" />
-        <select>
+      <form onSubmit={handleSumit}>
+        <input
+          type="text"
+          placeholder="Digite o título"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione uma Categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
